90a6bc624476b066a1a3449b208b5cca31d31752
吴名 <wuming497735138@gmail.com>
2018-09-28 18:37:32 +0800

    Merkle树

--- 比特币教程/Golang实现比特币/06-交易2/merkletree_test.go
+++ 比特币教程/Golang实现比特币/06-交易2/merkletree_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/hex"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,4 +46,30 @@ func Test_NewMerkleNode(t *testing.T) {
 		hex.EncodeToString(n7.Data),
 		"Root hash is correct",
 	)
+}
+
+func TestNewMerkleTree(t *testing.T) {
+	data := [][]byte{
+		[]byte("node1"),
+		[]byte("node2"),
+		[]byte("node3"),
+	}
+	// Level 1
+	n1 := NewMerkleNode(nil, nil, data[0])
+	n2 := NewMerkleNode(nil, nil, data[1])
+	n3 := NewMerkleNode(nil, nil, data[2])
+	n4 := NewMerkleNode(nil, nil, data[2])
+
+	// Level 2
+	n5 := NewMerkleNode(n1, n2, nil)
+	n6 := NewMerkleNode(n3, n4, nil)
+
+	// Level 3
+	n7 := NewMerkleNode(n5, n6, nil)
+
+	rootHash := fmt.Sprintf("%x", n7.Data)
+	mTree := NewMerkleTree(data)
+	actualHash := fmt.Sprintf("%x", mTree.RootNode.Data)
+
+	assert.Equal(t, rootHash, actualHash, "Merkle tree root hash is correct")
 }
