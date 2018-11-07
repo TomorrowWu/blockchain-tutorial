c8f6fdaccc01a171473e9df2ff2ac169e85b38ae
吴名 <wuming497735138@gmail.com>
2018-11-07 18:41:45 +0800

    基于token 的投票合约

--- 以太坊教程/token_based_voting_dapp/app/scripts/index.js
+++ 以太坊教程/token_based_voting_dapp/app/scripts/index.js
@@ -1,114 +1,132 @@
 // Import the page's CSS. Webpack will know what to do with it.
 import '../styles/app.css'
 // Import libraries we need.
-import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
 // Import our contract artifacts and turn them into usable abstractions.
-import metaCoinArtifact from '../../build/contracts/MetaCoin.json'
+import voting_artifacts from '../../build/contracts/Voting.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-const MetaCoin = contract(metaCoinArtifact)
+let Voting = contract(voting_artifacts)
+let candidates = {}
+let tokenPrice = null
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-let accounts
-let account
-
-const App = {
-  start: function () {
-    const self = this
-    
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider)
-    
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert('There was an error fetching your accounts.')
-        return
-      }
-      
-      if (accs.length === 0) {
-        alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.')
-        return
-      }
-      
-      accounts = accs
-      account = accounts[0]
-      
-      self.refreshBalance()
-    })
-  },
-  
-  setStatus: function (message) {
-    const status = document.getElementById('status')
-    status.innerHTML = message
-  },
-  
-  refreshBalance: function () {
-    const self = this
-    
-    let meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.getBalance.call(account, { from: account })
-    }).then(function (value) {
-      const balanceElement = document.getElementById('balance')
-      balanceElement.innerHTML = value.valueOf()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error getting balance; see log.')
-    })
-  },
-  
-  sendCoin: function () {
-    const self = this
-    
-    const amount = parseInt(document.getElementById('amount').value)
-    const receiver = document.getElementById('receiver').value
-    
-    this.setStatus('Initiating transaction... (please wait)')
-    
-    let meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.sendCoin(receiver, amount, { from: account })
+window.voteForCandidate = function (candidate) {
+  let candidateName = $('#candidate').val()
+  let voteTokens = $('#vote-tokens').val()
+  $('#msg').html('Vote has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.')
+  $('#candidate').val('')
+  $('#vote-tokens').val('')
+  Voting.deployed().then(function (contractInstance) {
+    contractInstance.voteForCandidate(candidateName, voteTokens, {
+      gas: 140000,
+      from: web3.eth.accounts[0]
     }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      let div_id = candidates[candidateName]
+      return contractInstance.totalVotesFor.call(candidateName).then(function (v) {
+        $('#' + div_id).html(v.toString())
+        $('#msg').html('')
+      })
+    })
+  })
+}
+
+window.buyTokens = function () {
+  let tokensToBuy = $('#buy').val()
+  let price = tokensToBuy * tokenPrice
+  $('#buy-msg').html('Purchase order has been submitted. Please wait.')
+  Voting.deployed().then(function (contractInstance) {
+    contractInstance.buy({ value: web3.toWei(price, 'ether'), from: web3.eth.accounts[0] }).then(function (v) {
+      $('#buy-msg').html('')
+      web3.eth.getBalance(contractInstance.address, function (err, result) {
+        $('#contract-balance').html(web3.fromWei(result.toString()) + ' Ether')
+      })
+    })
+  })
+  populateTokenData()
+}
+
+window.lookupVoterInfo = function () {
+  let address = $('#voter-info').val()
+  Voting.deployed().then(function (contractInstance) {
+    contractInstance.voterDetails.call(address).then(function (v) {
+      $('#tokens-bought').html('Total Tokens bought: ' + v[0].toString())
+      
+      let votesPerCandidate = v[1]
+      $('#votes-cast').empty()
+      $('#votes-cast').append('Votes cast per candidate: <br>')
+      let allCandidates = Object.keys(candidates)
+      for (let i = 0; i < allCandidates.length; i++) {
+        $('#votes-cast').append(allCandidates[i] + ': ' + votesPerCandidate[i] + '<br>')
+      }
+    })
+  })
+}
+
+function populateCandidates () {
+  Voting.deployed().then(function (contractInstance) {
+    contractInstance.allCandidates.call().then(function (candidateArray) {
+      for (let i = 0; i < candidateArray.length; i++) {
+        candidates[web3.toUtf8(candidateArray[i])] = 'candidate-' + i
+        setupCandidateRows()
+        populateCandidateVotes()
+        populateTokenData()
+      }
+    })
+  })
+}
+
+function populateCandidateVotes () {
+  let candidateNames = Object.keys(candidates)
+  for (let i = 0, len = candidateNames.length; i < len; i++) {
+    let name = candidateNames[i]
+    Voting.deployed().then(function (contractInstance) {
+      contractInstance.totalVotesFor.call(name).then(function (v) {
+        $('#' + candidates[name]).html(v.toString())
+      })
     })
   }
 }
 
-window.App = App
-
-window.addEventListener('load', function () {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn(
-      'Using web3 detected from external source.' +
-      ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
-      ' ensure you\'ve configured that source properly.' +
-      ' If using MetaMask, see the following link.' +
-      ' Feel free to delete this warning. :)' +
-      ' http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider)
-  } else {
-    console.warn(
-      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
-      ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
-      ' Consider switching to Metamask for development.' +
-      ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
-  }
+function setupCandidateRows () {
+  Object.keys(candidates).forEach(function (candidate) {
+    $('#candidate-rows').append('<tr><td>' + candidate + '</td><td id=\'' + candidates[candidate] + '\'></td></tr>')
+  })
   
-  App.start()
-})
+}
+
+function populateTokenData () {
+  Voting.deployed().then(function (contractInstance) {
+    contractInstance.totalTokens.call().then(function (v) {
+      $('#tokens-total').html(v.toString())
+    })
+    contractInstance.tokensSold.call().then(function (v) {
+      $('#tokens-sold').html(v.toString())
+    })
+    contractInstance.tokenPrice.call().then(function (v) {
+      tokenPrice = parseFloat(web3.fromWei(v.toString()))
+      $('#token-cost').html(tokenPrice + ' Ether')
+    })
+    web3.eth.getBalance(contractInstance.address, function (err, result) {
+      $('#contract-balance').html(web3.fromWei(result.toString()) + ' Ether')
+    })
+  })
+}
+
+const host = 'http://39.105.42.197:8545'
+// const host = 'http://localhost:8545';
+
+$(document).ready(function () {
+  if (typeof web3 !== 'undefined') {
+    console.warn('Using web3 detected from external source like Metamask')
+    // Use Mist/MetaMask's provider
+    // window.web3 = new Web3(web3.currentProvider);
+    window.web3 = new Web3(new Web3.providers.HttpProvider(host))
+  } else {
+    console.warn('No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider(host))
+  }
+  Voting.setProvider(web3.currentProvider)
+  populateCandidates()
+});
+
+
--- 以太坊教程/token_based_voting_dapp/truffle.js
+++ 以太坊教程/token_based_voting_dapp/truffle.js
@@ -4,9 +4,20 @@ require('babel-register')
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: '39.105.42.197', //39.105.42.197
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*', // Match any network id
+      gas: 10000, //如果你没有指定 2_deploy_contracts.js gas 值为 290000，migration 就会采用默认值 470000.
+      //from: your address //使用你指定的地址来部署和交互
+      // from:'0x6f456036724F33E39EC842782cE8bcFb2954c952',
+      gasPrice: 100
+    },
+    rinkeby: {
+      host: '39.105.42.197', // Connect to geth on the specified
+      port: 8545,
+      from: '0x6f456036724F33E39EC842782cE8bcFb2954c952', // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 }
