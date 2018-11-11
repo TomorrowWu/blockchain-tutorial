4137677338adad677d9a615bd7569c2f8c6fca16
吴名 <wuming497735138@gmail.com>
2018-11-11 21:52:54 +0800

    .sol

--- 以太坊教程/ebay_dapp/contracts/EcommerceStore.sol
+++ 以太坊教程/ebay_dapp/contracts/EcommerceStore.sol
@@ -28,6 +28,15 @@ contract EcommerceStore {
 		uint totalBids;
 		ProductStatus status;
 		ProductCondition condition;
+		//出价者地址 => {出价字符串 => 出价}
+		mapping(address => mapping(bytes32 => Bid)) bids;
+	}
+	
+	struct Bid {
+		address bidder; //出价者
+		uint productId;
+		uint value; //转账以太金额
+		bool revealed; //是否揭示
 	}
 	
 	constructor() public {
@@ -50,5 +59,84 @@ contract EcommerceStore {
 		Product memory product = stores[productIdInStore[_productId]][_productId];
 		return (product.id, product.name, product.category, product.imageLink, product.descLink, product.auctionStartTime,
 		product.auctionEndTime, product.startPrice, product.status, product.condition);
+	}
+	
+	function bid(uint _productId, bytes32 _bid) payable public returns (bool) {
+		Product storage product = stores[productIdInStore[_productId]][_productId];
+		require(now >= product.auctionStartTime);
+		require(now <= product.auctionEndTime);
+		require(msg.value > product.startPrice);
+		//本人对该商品未出过此价
+		require(product.bids[msg.sender][_bid].bidder == 0);
+		// msg.value 转账金额
+		product.bids[msg.sender][_bid] = Bid(msg.sender, _productId, msg.value, false);
+		product.totalBids += 1;
+		return true;
+	}
+	
+	function revealBid(uint _productId, string _amount, string _secret) public {
+		Product storage product = stores[productIdInStore[_productId]][_productId];
+		require(now > product.auctionEndTime);
+		bytes32 sealedBid = sha3(_amount, _secret);
+		
+		Bid memory bidInfo = product.bids[msg.sender][sealedBid];
+		require(bidInfo.bidder > 0);
+		require(bidInfo.revealed == false);
+		
+		uint refund;
+		
+		uint amount = stringToUint(_amount);
+		
+		if (bidInfo.value < amount) {
+			// They didn't send enough amount, they lost
+			refund = bidInfo.value;
+		} else {
+			// If first to reveal set as highest bidder
+			if (address(product.highestBidder) == 0) {
+				product.highestBidder = msg.sender;
+				product.highestBid = amount;
+				product.secondHighestBid = product.startPrice;
+				refund = bidInfo.value - amount;
+			} else {
+				if (amount > product.highestBid) {
+					product.secondHighestBid = product.highestBid;
+					product.highestBidder.transfer(product.highestBid);
+					product.highestBidder = msg.sender;
+					product.highestBid = amount;
+					refund = bidInfo.value - amount;
+				} else if (amount > product.secondHighestBid) {
+					product.secondHighestBid = amount;
+					refund = bidInfo.value;
+				} else {
+					refund = bidInfo.value;
+				}
+			}
+		}
+		product.bids[msg.sender][sealedBid].revealed = true;
+		
+		if (refund > 0) {
+			msg.sender.transfer(refund);
+		}
+	}
+	
+	function highestBidderInfo(uint _productId) view public returns (address, uint, uint) {
+		Product memory product = stores[productIdInStore[_productId]][_productId];
+		return (product.highestBidder, product.highestBid, product.secondHighestBid);
+	}
+	
+	function totalBids(uint _productId) view public returns (uint) {
+		Product memory product = stores[productIdInStore[_productId]][_productId];
+		return product.totalBids;
+	}
+	
+	function stringToUint(string s) pure private returns (uint) {
+		bytes memory b = bytes(s);
+		uint result = 0;
+		for (uint i = 0; i < b.length; i++) {
+			if (b[i] >= 48 && b[i] <= 57) {
+				result = result * 10 + (uint(b[i]) - 48);
+			}
+		}
+		return result;
 	}
 }
