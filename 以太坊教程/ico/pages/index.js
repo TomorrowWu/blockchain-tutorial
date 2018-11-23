e8b0faecbd1f41ce6fcb74c0657951bd04e7e749
wuming <wuming.coding@gmail.com>
2018-11-23 14:00:34 +0800

    1,ico_dapp header layout

--- 以太坊教程/ico/pages/index.js
+++ 以太坊教程/ico/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Button} from "@material-ui/core";
-import withRoot from '../libs/withRoot'
+import withRoot from '../libs/withRoot';
+import Layout from '../components/Layout';
 
 class Index extends React.Component {
 	render() {
-		return <Button variant="raised" color="primary">Welcome to Ethereum ICO DApp!</Button>;
+		return (
+			<Layout>
+                 <Button variant="raised" color="primary">
+                     Welcome to Ethereum ICO DApp!
+                 </Button>
+            </Layout>
+		)
 	}
 }
+
 
 export default withRoot(Index);
